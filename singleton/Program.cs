38599e2a7c5c6422441ea9fed5b4dbf84005659a
Tran, Phuoc (Jon) <60534054+ttnnphuoc@users.noreply.github.com>
2023-10-22 17:17:35 +0700

    Implement singleton

---
+++ singleton/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using singleton;
+
+//Singleton s1 = Singleton.GetInstance();
+//Singleton s2 = Singleton.GetInstance();
+
+//if (s1 == s2)
+//{
+//    Console.WriteLine("Singleton works, both variables contain the same instance.");
+//}
+//else
+//{
+//    Console.WriteLine("Singleton failed, variables contain different instances.");
+//}
+
+TestSingletonThread();
+
+static void TestSingletonThread()
+{
+    // The client code.
+
+    Console.WriteLine(
+        "{0}\n{1}\n\n{2}\n",
+        "If you see the same value, then singleton was reused (yay!)",
+        "If you see different values, then 2 singletons were created (booo!!)",
+        "RESULT:"
+    );
+
+    Thread process1 = new Thread(() =>
+    {
+
+        SingletonThread singleton = SingletonThread.GetInstance("FOO");
+        Console.WriteLine(singleton.Value);
+    });
+
+    Thread process2 = new Thread(() =>
+    {
+        SingletonThread singleton = SingletonThread.GetInstance("BAR");
+        Console.WriteLine(singleton.Value);
+    });
+
+    process1.Start();
+    process2.Start();
+
+    process1.Join();
+    process2.Join();
+}
+
+static void TestSingleton()
+{
+    // The client code.
+    Singleton s1 = Singleton.GetInstance();
+    Singleton s2 = Singleton.GetInstance();
+
+    if (s1 == s2)
+    {
+        Console.WriteLine("Singleton works, both variables contain the same instance.");
+    }
+    else
+    {
+        Console.WriteLine("Singleton failed, variables contain different instances.");
+    }
+}
---
+++ singleton/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    //It’s pretty easy to implement a sloppy Singleton. You just need to hide the constructor and implement a static creation method.
+    // The same class behaves incorrectly in a multithreaded environment.
+    // Multiple threads can call the creation method simultaneously and get several instances of Singleton class.
+
+    // The Singleton class defines the `GetInstance` method that serves as an
+    // alternative to constructor and lets clients access the same instance of this class over and over.
+
+    // EN : The Singleton should always be a 'sealed' class to prevent class
+    // inheritance through external classes and also through nested classes.
+    public sealed class Singleton
+    {
+        // The Singleton's constructor should always be private to prevent direct construction calls with the `new` operator.
+        private Singleton() { }
+
+        // The Singleton's instance is stored in a static field. There there are multiple ways to initialize this field, all of them have various pros and cons.
+        // In this example we'll show the simplest of these ways, which, however, doesn't work really well in multithreaded program.
+        private static Singleton _instance;
+
+        // This is the static method that controls the access to the singletoninstance.
+        // On the first run, it creates a singleton object and places it into the static field.
+        // On subsequent runs, it returns the client existing object stored in the static field.
+        
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
